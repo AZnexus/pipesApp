6427bfcc6e4e47eacc88092fa280463557514d8c
Alex Zarauza <azarauza@nexusgeographics.com>
2024-03-11 16:59:09 +0100

    Feature - Custom pipes + finish project

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -2,21 +2,26 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ProductsRoutingModule } from './products-routing.module';
+
 import { BasicsPageComponent } from './pages/basics-page/basics-page.component';
+import { CanFlyPipe } from './pipes/can-fly.pipe';
 import { NumbersPageComponent } from './pages/numbers-page/numbers-page.component';
 import { UncommonPageComponent } from './pages/uncommon-page/uncommon-page.component';
 import { PrimeNgModule } from '../prime-ng/prime-ng.module';
 import { OrderComponent } from './pages/order/order.component';
+import { SortByPipe } from './pipes/sort-by.pipe';
 import { ToggleCasePipe } from './pipes/toggle-case.pipe';
 
 
 @NgModule({
   declarations: [
     BasicsPageComponent,
+    CanFlyPipe,
     NumbersPageComponent,
-    UncommonPageComponent,
     OrderComponent,
-    ToggleCasePipe
+    SortByPipe,
+    ToggleCasePipe,
+    UncommonPageComponent,
   ],
   imports: [
     CommonModule,
